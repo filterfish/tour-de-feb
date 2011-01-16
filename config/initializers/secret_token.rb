f5201fcb54bd9d58baac0657bc2c8169c67edd50
Richard Heycock <rgh@roughage.com.au>
2011-01-16 12:59:25 +1100

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TourDeFeb::Application.config.secret_token = '61046c4d1915b31584c56f9c7ef81447ca44e18460d9a86cb4a610d8631ae3008e2cd520008fb2e000379edf4cf54d88fc0256c4e5233caa8629ebf27aa16338'
