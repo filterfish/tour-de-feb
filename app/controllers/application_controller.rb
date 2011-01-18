404a09d0cf5ffb6e04d1271139ec5d509e6ec49e
Richard Heycock <rgh@roughage.com.au>
2011-01-18 20:03:59 +1100

    Redirect to rides after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_time_zone
 
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a? User
+      rides_path
+    else
+      super
+    end
+  end
+
   def set_time_zone
     if current_user
       pp current_user
