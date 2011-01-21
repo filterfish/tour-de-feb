df9c88b9959b1e10d5e102c3d5af391a2b34afb3
Richard Heycock <rgh@roughage.com.au>
2011-01-21 16:02:34 +1100

    Add ride validation

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,10 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :date
+
+  def validate
+    if length.nil? && duration.nil?
+      errors.add_to_base("You must supply either a distance or duration")
+    end
+  end
 end
