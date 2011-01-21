1b7a58104f9b840f8a622f76cb0dd141a4b0b156
Richard Heycock <rgh@roughage.com.au>
2011-01-21 16:02:06 +1100

    Add flash instructions on first login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a? User
+      if current_user.sign_in_count == 1
+        flash[:notice] = "Don't forget to update goals & other details: <a href='#{edit_preferences_url}'>preferences</a>".html_safe
+      end
       rides_path
     else
       super
@@ -13,7 +16,6 @@ class ApplicationController < ActionController::Base
 
   def set_time_zone
     if current_user
-      pp current_user
       Time.zone = current_user.preference.time_zone || "UTC"
     else
       Time.zone = "UTC"
