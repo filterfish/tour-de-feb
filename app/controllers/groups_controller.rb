89ebb6838d51e2e896286a529bc004ae285d9789
Richard Heycock <rgh@roughage.com.au>
2011-01-29 16:36:48 +1100

    Check a user has permissions to see a group. Clean up groups controller.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,39 +5,11 @@ class GroupsController < ApplicationController
     @group = Group.where(['name = ?', params[:name]]).first
     @users = @group.try(:users)
 
-    if @users && @users.size > 0
+    if @users && @users.size > 0 && current_user.group.name == params[:name]
       @kms_miles_or_both = get_distance_metric(@users)
     else
       flash[:error] = "Sorry that group doesn't exist"
       redirect_to rides_url
-    end
-  end
-
-  def new
-    @ride = Ride.new
-  end
-
-  def edit
-    @ride = current_user.rides.find(params[:id])
-  end
-
-  def create
-    @ride = current_user.rides.new(params[:ride])
-
-    if @ride.save
-      redirect_to rides_url
-    else
-      render :action => "new"
-    end
-  end
-
-  def update
-    @ride = current_user.rides.find(params[:id])
-
-    if @ride.update_attributes(params[:ride])
-      redirect_to rides_url
-    else
-      render :action => "edit"
     end
   end
 
