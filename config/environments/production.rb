04ccc010b01aeb26ea7a486a4e8998c60e958c73
Richard Heycock <rgh@roughage.com.au>
2011-02-02 00:32:58 +1100

    Configure action mailer to use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,8 @@ TourDeFeb::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :sendmail
+
   # Enable threaded mode
   # config.threadsafe!
 
